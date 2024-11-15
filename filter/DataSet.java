fc73067709bb0cb9b5324358b743d709a3885622
cmcvoy <cm1019396@wcupa.edu>
2024-11-14 19:15:00 -0500

    Update DataSet.java

--- filter/DataSet.java
+++ filter/DataSet.java
@@ -1,6 +1,27 @@
 package filter;
 import java.util.ArrayList;
 
-public abstract class DataSet {
+public class DataSet {
     protected ArrayList<Email> emails;
+
+    public DataSet(ArrayList<Email> emails) {
+        this.emails = emails;
+    }
+
+    /**
+     * Get the Email at given index
+     * @param index - the index of the email you want
+     * @return The Email at index <b>index</b>
+     */
+    public Email getEmail(int index) {
+        return emails.get(index);
+    }
+
+    /**
+     * Get the size of the DataSet
+     * @return The number of emails in the set
+     */
+    public int size() {
+        return emails.size();
+    }
 }
